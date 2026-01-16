1a16c5215881b47c9477c8da4e44f73e4a341bb2
anamolcancode <anamol.bhattarai@nepalcancode.com>
2026-01-16 12:50:18 +0545

    added icons in the prcing screen's dropdowns

--- constants/dropdowns.ts
+++ constants/dropdowns.ts
@@ -1,5 +1,4 @@
 import { MaterialIcons } from "@expo/vector-icons";
-
 interface IconConfig {
   as: typeof MaterialIcons;
   name: keyof typeof MaterialIcons.glyphMap;
@@ -17,12 +16,10 @@ export const DocumentServiceType: DropdownItem[] = [
   { 
     _id: "Express", 
     name: "Express",
-    icon: { as: MaterialIcons, name: "flash-on", size: 16, color: "#666" }
   },
   { 
     _id: "Standard", 
     name: "Standard",
-    icon: { as: MaterialIcons, name: "schedule", size: 16, color: "#666" }
   },
 ];
 
@@ -30,12 +27,10 @@ export const ParcelServiceType: DropdownItem[] = [
   { 
     _id: "Express", 
     name: "Express",
-    icon: { as: MaterialIcons, name: "flash-on", size: 16, color: "#666" }
   },
   { 
     _id: "Economy", 
     name: "Economy",
-    icon: { as: MaterialIcons, name: "savings", size: 16, color: "#666" }
   },
 ];
 
@@ -43,12 +38,10 @@ export const ShipmentType: DropdownItem[] = [
   { 
     _id: "Document", 
     name: "Document",
-    icon: { as: MaterialIcons, name: "description", size: 16, color: "#666" }
   },
   { 
     _id: "Parcel", 
     name: "Parcel",
-    icon: { as: MaterialIcons, name: "inventory-2", size: 16, color: "#666" }
   },
 ];
 
